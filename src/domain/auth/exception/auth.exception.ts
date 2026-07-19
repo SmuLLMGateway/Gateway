558604d48c567fab317c5cd2709aed817b6f7b02
JuHeon <juheon0313@naver.com>
2026-07-20 00:41:07 +0900

    ✨ feat: AuthException 클래스를 BlockchainException에서 GatewayException으로 변경

--- src/domain/auth/exception/auth.exception.ts
+++ src/domain/auth/exception/auth.exception.ts
@@ -1,7 +1,7 @@
 import { BaseStatus } from "../../../global/apiPayload/code/status.js";
-import { BlockchainException } from "../../../global/apiPayload/exception/blockchain.exception.js";
+import { GatewayException } from "../../../global/apiPayload/exception/gateway.exception.js";
 
-export class AuthException extends BlockchainException {
+export class AuthException extends GatewayException {
     constructor(code: BaseStatus) {
         super(code)
     }
