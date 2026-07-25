5301daa42fed7fedf079e78b83503ebd2af136ec
JuHeon <juheon0313@naver.com>
2026-07-25 22:38:01 +0900

    ♻️ refactor: Redis 관련 구성 요소 및 설정 삭제

--- src/global/redis/module/redis.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RedisConfig } from '../config/redis.config.js';
-import { RedisService } from '../service/redis.service.js';
-
-@Module({
-  providers: [RedisConfig, RedisService],
-  exports: [RedisService],
-})
-export class RedisModule {}
